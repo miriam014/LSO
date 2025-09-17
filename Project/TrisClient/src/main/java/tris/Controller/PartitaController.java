af4a0a2e3ec2c5d6aee9b5ae852fbc8139d22ecb
Tatuuz <for.durso@studenti.unina.it>
2025-09-17 19:49:50 +0200

    inizio gestione partita

--- Project/TrisClient/src/main/java/tris/Controller/PartitaController.java
+++ Project/TrisClient/src/main/java/tris/Controller/PartitaController.java
@@ -1,11 +1,38 @@
 package tris.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import tris.Main;
 
 public class PartitaController {
-    public Button back;
+    @FXML public Button back;
+    @FXML private Label labelResult;
+    @FXML private Button replayButton;
+
+    @FXML private Button btn00;
+    @FXML private Button btn01;
+    @FXML private Button btn02;
+    @FXML private Button btn10;
+    @FXML private Button btn11;
+    @FXML private Button btn12;
+    @FXML private Button btn20;
+    @FXML private Button btn21;
+    @FXML private Button btn22;
+
+
+    @FXML
+    public void inizialize() {
+        labelResult.setVisible(false);
+        replayButton.setVisible(false);
+        replayButton.setManaged(false);
+    }
+
+    @FXML
+    private void handleMove(ActionEvent e) {
+        
+    }
 
     public void backHome(ActionEvent actionEvent) {
         try {
